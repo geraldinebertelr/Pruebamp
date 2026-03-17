0779f8c0ecd5ddbcbe76fd3b6776f767c0dfee99
Geraldine Bertel Romero <gbertel@uninorte.edu.co>
2026-03-17 15:31:16 -0500

    Change column count from 6 to 5 in app.py

--- app.py
+++ app.py
@@ -631,10 +631,10 @@ elif menu == "Inventarios":
     # -----------------------------------
     st.markdown("### Estado por espacio")
 
-    cols = st.columns(6)
+    cols = st.columns(5)
 
     for i, e in enumerate(espacios):
-        with cols[i % 6]:
+        with cols[i % 5]:
 
             st.markdown(f"#### 📦 {e['espacio']}")
 
